fb7aac5230810505068b1ef9ee59b269ac5fb6a4
vickneee <victoria.vavulina@outlook.com>
2023-05-26 21:23:26 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const togglebtn = document.querySelector('.togglebtn');
+
+togglebtn.addEventListener('click', (e) => {
+    const html = document.querySelector('html');
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark');
+        e.target.innerHTML = 'Dark mode';
+    } else {
+        html.classList.add('dark');
+        e.target.innerHTML = 'Light mode';
+    }
+});
